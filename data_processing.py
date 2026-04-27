1c20be6fa6d0f4b101cf3ac1264e0e1b4e5b4925
damecodes <dams9713@gmail.com>
2026-04-26 23:13:45 -0400

    data processing fixes

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,11 @@
+import pandas as pd
+
+def compute_gdp_growth(df):
+    df = df.copy()
+    df["GDP_Growth"] = df["GDP"].pct_change() * 400
+    return df
+
+
 def compute_feature_growth(df):
     df = df.copy()
 
@@ -5,7 +13,7 @@ def compute_feature_growth(df):
     df["Industrial_Production"] = df["Industrial_Production"].pct_change() * 100
     df["Retail_Sales"] = df["Retail_Sales"].pct_change() * 100
 
-    # unemployment → difference, not pct change
+    # unemployment → use difference, not pct change
     df["Unemployment"] = df["Unemployment"].diff()
 
     return df
